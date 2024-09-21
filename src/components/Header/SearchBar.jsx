3b247ce135727aa104e633e6c45f06043a36b823
ROrtegaAyala <ortegaayalarolando@gmail.com>
2024-09-21 00:52:58 -0500

    Se realizo el Header  y la parte que lleva al formulario. instalar react-icons/fa

---
+++ src/components/Header/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './Header.css';
+
+function SearchBar() {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    window.location.reload(); // Recarga la página al enviar
+  };
+
+  return (
+    <form className="search-bar" onSubmit={handleSearch}>
+      <input 
+        type="text" 
+        placeholder="Buscar" 
+        value={query} 
+        onChange={(e) => setQuery(e.target.value)} 
+      />
+      <button type="submit">🔍</button>
+    </form>
+  );
+}
+
+export default SearchBar;
