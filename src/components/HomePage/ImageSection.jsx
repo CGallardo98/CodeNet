67c043ffb124324f4bcb86d93536beeeafa7cfad
ROrtegaAyala <ortegaayalarolando@gmail.com>
2024-09-23 11:32:13 -0500

    Cambio imagen Logo-SectionImage

--- src/components/HomePage/ImageSection.jsx
+++ src/components/HomePage/ImageSection.jsx
@@ -7,7 +7,7 @@ const ImageSection = () => {
     <div className="image-section-container">
       <img src="https://i.postimg.cc/FzCz3Vfc/intro3.png" alt="Main" className='left-image'/>
       <div className="right-image-section">
-        <img src="https://i.ibb.co/wggkRsK/image-Photoroom.png" alt="Codenet Logo" className="logo-image2"/>
+        <img src="https://i.postimg.cc/SR5FVMV3/image-Photoroom.png" alt="Codenet Logo" className="logo-image2"/>
         <div className="text-below-logo">
         </div>
         <Link to="/entry/new">
